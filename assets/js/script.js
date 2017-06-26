394713792e44048a8a4bfec0ca39c56e43e95be0
ppalyga <homme.ex.machina@gmail.com>
2017-06-26 21:44:27 +0200

    ADD menu carousel

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,22 @@
-(function () {
+$(document).ready(function () {
 
-    let menuBtn = document.querySelector('.menu--hidden__btn'),
-        menu = document.querySelector('.menu--visible');
+    let menuBtn = $('.menu--hidden__btn'),
+        menu = $('.menu--visible');
 
-    menuBtn.addEventListener('click', function () {
-        if (menu.style.display === 'none') {
-            menu.style.display = 'block';
-            menu.style.opacity = '1';
-        } else {
-            menu.style.display = 'none';
-            menu.style.opacity = '0';
-        }
-    }, false);
+    menuBtn.on('click', function () {
+        menu.fadeToggle();
+    });
 
-})();
+    $(".menu--visible__carousel").flipster({
+        style: 'flat',
+        spacing: 0,
+        loop: true,
+        click: true,
+        keyboard: true,
+        scrollwheel: true,
+        touch: true,
+        fadeIn: 100
+    });
+
+});
+
