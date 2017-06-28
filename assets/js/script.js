874dcde065fe08268dc8ef2c126b35602ceb1fb8
ppalyga <homme.ex.machina@gmail.com>
2017-06-28 21:19:07 +0200

    ADD skill cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,8 @@ $(document).ready(function () {
         menuHidden = $('.menu--hidden'),
         menu = $('.menu--visible'),
         menuCarousel = document.querySelector('.menu--visible__carousel'),
-        menuList = document.querySelector('menu--visible__list'),
-        menuItems = $('.menu--visible__list-item');
+        menuItems = $('.menu--visible__list-item'),
+        skillsList = document.querySelector('.skills__list');
 
     menuHidden.css('display', 'flex');
     // click events
@@ -29,7 +29,49 @@ $(document).ready(function () {
         touch: true,
         fadeIn: 100
     });
-    // touch support
+
+    // skills cards
+    // mark the first and the last card
+    var cards = document.querySelectorAll('.skills__card'),
+        cardsArray = [];
+    Array.prototype.forEach.call(cards, function (card) {
+        cardsArray.push(card);
+    });
+
+    cardsArray[0].classList.add('skills__card--first');
+    cardsArray[cardsArray.length - 1].classList.add('skills__card--last');
+
+    var skillsFirstCard = $('.skills__card--first'),
+        skillsLastCard = $('.skills__card--last');
+
+    // add touch support for the cards
+    var skillsSlide = new Hammer(skillsList);
+
+    skillsSlide.on('swipeleft', function () {
+        var skillsActiveCard = $('.skills__card--active');
+
+        if (skillsActiveCard.hasClass('skills__card--last')) {
+            skillsActiveCard.toggleClass('skills__card--active');
+            skillsFirstCard.toggleClass('skills__card--active');
+        } else {
+            skillsActiveCard.toggleClass('skills__card--active');
+            skillsActiveCard.next().toggleClass('skills__card--active');
+        }
+    });
+
+    skillsSlide.on('swiperight', function () {
+        var skillsActiveCard = $('.skills__card--active');
+
+        if (skillsActiveCard.hasClass('skills__card--first')) {
+            skillsActiveCard.toggleClass('skills__card--active');
+            skillsLastCard.toggleClass('skills__card--active');
+        } else {
+            skillsActiveCard.toggleClass('skills__card--active');
+            skillsActiveCard.prev().toggleClass('skills__card--active');
+        }
+    });
+
+    // add touch support for the menu
     var menuSlide = new Hammer(menuCarousel);
 
     menuSlide.on('swipeleft', function () {
