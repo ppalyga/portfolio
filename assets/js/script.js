a94bd2161366a56132175ccf769e298c6afd223d
ppalyga <homme.ex.machina@gmail.com>
2017-06-28 22:35:36 +0200

    IMPROVE script.js structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,61 @@
 $(document).ready(function () {
 
-    // MENU
-    // variables
-    var menuBtn = $('.menu--hidden__btn'),
-        menuHidden = $('.menu--hidden'),
-        menu = $('.menu--visible'),
-        menuCarousel = document.querySelector('.menu--visible__carousel'),
-        menuItems = $('.menu--visible__list-item'),
-        skillsList = document.querySelector('.skills__list');
+    function initializeMenu() {
+        var menuHidden = $('.menu--hidden');
+        menuHidden.css('display', 'flex');
+        addMenuClickEvents();
+        initializeCarousel();
+    }
 
-    menuHidden.css('display', 'flex');
-    // click events
-    menuBtn.on('click', function () {
-        menu.fadeToggle();
-    });
+    function addMenuClickEvents() {
+        var menu = $('.menu--visible'),
+            menuBtn = $('.menu--hidden__btn'),
+            menuItems = $('.menu--visible__list-item');
 
-    menuItems.on('click', function () {
-        menu.fadeToggle();
-    });
-    // carousel
-    var carousel = $(".menu--visible__carousel").flipster({
-        style: 'flat',
-        spacing: 0,
-        loop: true,
-        click: true,
-        keyboard: true,
-        scrollwheel: true,
-        touch: true,
-        fadeIn: 100
-    });
+        menuBtn.on('click', function () {
+            menu.fadeToggle();
+        });
 
-    // add touch support for the menu
-    var menuSlide = new Hammer(menuCarousel);
+        menuItems.on('click', function () {
+            menu.fadeOut();
+        });
 
-    menuSlide.on('swipeleft', function () {
-        carousel.flipster('prev');
-    });
-    menuSlide.on('swiperight', function () {
-        carousel.flipster('next');
-    });
+        menu.on('click', function () {
+            menu.fadeOut();
+        });
+    }
 
-    $('.skills__list').slick({
-        dots: true,
-        arrows: false
-    });
+    function initializeCarousel() {
+        var carousel = $(".menu--visible__carousel").flipster({
+            style: 'flat',
+            spacing: 0,
+            loop: true,
+            click: true,
+            keyboard: true,
+            scrollwheel: true,
+            touch: true,
+            fadeIn: 100
+        }),
+            menuCarousel = document.querySelector('.menu--visible__carousel'),
+            menuSlide = new Hammer(menuCarousel);
+
+        menuSlide.on('swipeleft', function () {
+            carousel.flipster('prev');
+        });
+        menuSlide.on('swiperight', function () {
+            carousel.flipster('next');
+        });
+    }
+
+    function initilizeSkills() {
+        $('.skills__list').slick({
+            dots: true,
+            arrows: false
+        });
+    }
+
+    initializeMenu();
+    initilizeSkills();
 });
 
 
